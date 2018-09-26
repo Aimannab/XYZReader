33c929f054593e078f717130df50fe0091c69bc9
Aiman <aiman.nabeel@gmail.com>
2018-09-26 12:54:10 +0100

    Updating FAB and NestedScrollView

--- XYZReader/src/main/java/com/example/xyzreader/ui/NestedScrollView.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/NestedScrollView.java
@@ -1,34 +1,13 @@
-/*
- * Copyright 2012 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.xyzreader.ui;
 
 import android.content.Context;
-import android.os.Parcelable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ScrollView;
 
-/**
- * A custom ScrollView that can accept a scroll listener.
- */
-public class ObservableScrollView extends ScrollView {
-    private Callbacks mCallbacks;
+public class NestedScrollView extends ScrollView {
+    private NestedScrollView.Callbacks mCallbacks;
 
-    public ObservableScrollView(Context context, AttributeSet attrs) {
+    public NestedScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
@@ -55,7 +34,7 @@ public class ObservableScrollView extends ScrollView {
         return super.computeVerticalScrollRange();
     }
 
-    public void setCallbacks(Callbacks listener) {
+    public void setCallbacks(NestedScrollView.Callbacks listener) {
         mCallbacks = listener;
     }
 
